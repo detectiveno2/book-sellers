0b4afef42c5096b2fd568e120ebef73e09003d60
detectiveno2 <nhatthien185@gmail.com>
2021-08-11 20:38:09 +0700

    setup styles, font

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 import { Provider } from 'react-redux'
 import store from './states/store'
+import './styles/app.scss'
 
 ReactDOM.render(
   <React.StrictMode>
