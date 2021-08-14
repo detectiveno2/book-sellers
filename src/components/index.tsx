5d9bbace458ae3546c5626aae9fc1d943b2eafb6
detectiveno2 <nhatthien185@gmail.com>
2021-08-14 21:35:00 +0700

    ModifiedButton component and index file in components folder

---
+++ src/components/index.tsx
@@ -0,0 +1,3 @@
+export { default as Avatar } from './Avatar/Avatar'
+export { default as ModifiedButton } from './ModifiedButton/ModifiedButton'
+export { default as PrimaryButton } from './PrimaryButton/PrimaryButton'
