ad6d2f5db6590ba75fa7ed964029f5a851c90422
detectiveno2 <nhatthien185@gmail.com>
2021-08-16 22:00:12 +0700

    add homepage and test pages, update route for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,20 @@
-import { useState } from 'react'
-import { useAppDispatch, useAppSelector } from './states/hooks'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
 
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-} from './states/actions/counter.action'
-import { Avatar, ModifiedButton, PrimaryButton } from './components'
+import { PrivateRoute, PublicRoute } from './routes'
+import { HomePage, Test } from './pages'
 
 import './App.scss'
-import { LikeActiveIcon, LikeIcon } from './assets/svgs'
-
-interface IState {
-  input: string
-}
 
 const App: React.FC = () => {
-  const [input, setInput] = useState<IState['input']>('')
-  const [testModifiedBtn, setTestModifiedBtn] = useState<boolean>(false)
-  const dispatch = useAppDispatch()
-  const counter = useAppSelector(state => state.counter)
-
-  const handleDecrementClick = () => {
-    dispatch(decrement())
-  }
-
-  const handleIncrementClick = () => {
-    dispatch(increment())
-  }
-
-  const handleIncrementByAmountClick = () => {
-    dispatch(incrementByAmount(parseInt(input)))
-  }
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInput(e.target.value)
-  }
-
-  const handleTestModifiedBtnClick = () => {
-    setTestModifiedBtn(!testModifiedBtn)
-  }
-
   return (
-    <div className='App'>
-      {/* Test font */}
-      <div>
-        <span className='font-bold size-20 cl_amaranth'>test font</span>
+    <Router>
+      <div className='App'>
+        <Switch>
+          <PrivateRoute path='/' exact={true} component={HomePage} />
+          <PublicRoute path='/test' exact={true} component={Test} />
+        </Switch>
       </div>
-
-      {/* Test redux */}
-      <div>
-        <span>{counter.value}</span>
-        <button onClick={handleDecrementClick}>-</button>
-        <button onClick={handleIncrementClick}>+</button>
-        <input
-          type='text'
-          name='amount'
-          value={input}
-          onChange={handleChange}
-        />
-        <button onClick={handleIncrementByAmountClick}>add by amount</button>
-      </div>
-
-      {/* Test PrimaryButton component */}
-      <div>
-        <PrimaryButton text='Click Me' handleClick={handleIncrementClick} />
-      </div>
-
-      {/* Test Avatar component */}
-      <div>
-        <Avatar src={'https://loremflickr.com/320/240'} />
-      </div>
-
-      {/* Test ModifiedButton component */}
-      <div>
-        <a href='#'>
-          <ModifiedButton
-            activeIcon={LikeActiveIcon}
-            disableIcon={LikeIcon}
-            condition={testModifiedBtn}
-            handleClick={handleTestModifiedBtnClick}
-          />
-        </a>
-      </div>
-    </div>
+    </Router>
   )
 }
 
---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,7 @@
+import './HomePage.scss'
+
+const HomePage: React.FC = () => {
+  return <div className='home-page'>homepage</div>
+}
+
+export default HomePage
